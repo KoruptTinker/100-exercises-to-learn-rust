025124f6db0f948b4961bb1ea332f603498e47e5
KoruptTinker <pisces1237@gmail.com>
2024-06-11 20:18:13 +0530

    Add: Part4 Solns

--- exercises/04_traits/08_from/src/lib.rs
+++ exercises/04_traits/08_from/src/lib.rs
@@ -4,6 +4,14 @@ pub struct WrappingU32 {
     value: u32,
 }
 
+impl From<u32> for WrappingU32 {
+    fn from(n: u32) -> Self {
+        return WrappingU32{
+            value: n,
+        };
+    }
+}
+
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
